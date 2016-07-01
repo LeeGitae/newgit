ca8e050e134bb86197a66f67834b010420222dca
LeeGitae <mm1023@naver.com>
2016-07-01 10:13:08 +0900

    테스트

---
+++ src/main/java/com/leegitae/newproj/App.java
@@ -0,0 +1,13 @@
+package com.leegitae.newproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
